f39238d658169c6ab92554f47f18e8551c8054ff
americanman <snowboardracer7@gmail.com>
2018-09-06 19:25:34 -0400

    Fix body/footer padding

--- js/main.js
+++ js/main.js
@@ -197,7 +197,22 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 
-} 
+}
+
+document.querySelectorAll('#cuisines-select, #neighborhoods-select').forEach((elem) => {
+  elem.addEventListener('change', () => {
+    const list = document.getElementById('restaurants-list')
+    setTimeout(function() {
+      if(list.childNodes.length === 0) {
+        list.style.padding = 0;
+      }
+      else {
+        list.style.padding = '30px 15px 60px';
+      }
+    },100)
+  })
+})
+
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
